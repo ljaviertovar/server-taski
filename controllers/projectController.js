b633ff4b9082442aa288ffd7c6aea3d11bfbcc7d
ljaviertovar <luisjavier_tovar@outlook.com>
2021-10-11 21:44:36 -0500

    :sparkles: [ADD] Endpoint to update porject

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,19 +1,83 @@
 const Project = require('../models/Project');
+const { validationResult } = require('express-validator');
+const { exists } = require('../models/Project');
 
-exports.createProject = async(req, res) => {
+exports.createProject = async (req, res) => {
+
+    //valid error 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
     try {
-        
+
         // creat project
-        const project = new Project(req.body); 
+        const project = new Project(req.body);
         // save creator by jwt 
-        project.creator= req.user.id;
+        project.creator = req.user.id;
 
         project.save();
         res.json(project)
 
     } catch (error) {
         console.error(error);
+        res.status(500).send('There was an error');
+    }
+
+}
+
+// get all project of actual user
+exports.getProjects = async (req, res) => {
+
+    try {
+
+        const projects = await Project.find({ creator: req.user.id }).sort({ created_at: -1 });
+        res.json({ projects });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('There was an error');
+    }
+
+}
+
+// update a project
+exports.updateProject = async (req, res) => {
+
+    //valid error 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name } = req.body;
+    if (name) {
+        const newProject = {};
+        newProject.name = name;
+    }
+
+    try {
+
+        // check ID 
+        let project = await Project.findById(req.params.id);
+
+        // if project exists 
+        if (!project) {
+            return res.status(404).json({ msg: 'Project not found' });
+        }
+        // verify project creator 
+        if (project.creator.toString() !== req.user.id) {
+            return res.status(404).json({ msg: 'User no authorized' });
+        }
+
+        // update 
+        project = await Project.findByIdAndUpdate({ _id: req.params.id }, { $set: newProject }, { new: true });
+
+        res.json({ project });
+
+    } catch (error) {
+        console.log(error);
         res.status(500).send('There was an error');
     }
 
--- routes/projects.js
+++ routes/projects.js
@@ -2,16 +2,30 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 //  create projects api/projects
 router.post('/',
     auth,
+    [
+        check('name', 'Project name is required.').not().isEmpty()
+    ],
     projectController.createProject
 );
 
+// get projects
 router.get('/',
     auth,
-    projectController.createProject
+    projectController.getProjects
+);
+
+// update project
+router.put('/:id',
+    auth,
+    [
+        check('name', 'Name projects is required').not().isEmpty()
+    ],
+    projectController.updateProject
 );
 
 
